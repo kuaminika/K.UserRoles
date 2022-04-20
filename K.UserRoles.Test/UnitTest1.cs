5fb0e6aabf2dbb499ed90033ade68bf228d5e304
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2022-04-19 23:30:59 -0400

    adding Test project

---
+++ K.UserRoles.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace K.UserRoles.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
